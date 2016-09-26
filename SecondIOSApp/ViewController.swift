8d769fa0101dd0ea8a281e6882f84d40760b3f84
kmart22 <kmar7070@740s-pl-111.local>
2016-09-26 08:55:29 -0600

    tried GUI.
    added @IBOutlet and @IBAction to link the viewcontroller

--- SecondIOSApp/ViewController.swift
+++ SecondIOSApp/ViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var firstButton: UIButton!
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
+
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func IbAction(sender: UIButton)
+    {
+        textLabel.text = "you clicked me"
+        textLabel.textColor = UIColor.cyanColor()
+    }
 }
 
